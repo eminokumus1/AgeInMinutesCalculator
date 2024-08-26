5d4bd1b9f481e21fbae29ddd9d2dddac1a40727b
eminokumus1 <eminokumus1@gmail.com>
2024-08-26 16:40:35 +0300

    DatePickerDialog added

--- app/src/main/java/com/eminokumus/ageinminutescalculator/MainActivity.kt
+++ app/src/main/java/com/eminokumus/ageinminutescalculator/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.eminokumus.ageinminutescalculator
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eminokumus.ageinminutescalculator.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.selectDateBtn.setOnClickListener {
+            clickDatePicker()
+        }
     }
+    private fun clickDatePicker(){
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        //month index starts with zero, so we add one
+        val month = calendar.get(Calendar.MONTH)+1
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            { view, selectedYear, selectedMonth, selectedDay ->
+                val selectedDateStr = "$selectedDay/$selectedMonth/$selectedYear"
+                binding.dateText.text = selectedDateStr
+
+
+            },
+            year,
+            month,
+            day
+        ).show()
+    }
+
+
 }
